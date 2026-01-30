67395e5b92a4989128be10320f29cabfb66d6d6d
gracemathew-2004 <jaigraceg@gmail.com>
2026-01-30 18:44:07 +0530

    Initialize Streamlit app for Smart Parking System
    Added initial Streamlit setup and app title with success message.

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(page_title="Smart Parking App", layout="wide")
+
+st.title("🚗 Smart Parking & Theft Alert System")
+st.success("App deployed successfully!")
+
+st.write("Next steps:")
+st.write("- Vehicle detection")
+st.write("- Suspicious activity detection")
+st.write("- WhatsApp / SMS alert")
